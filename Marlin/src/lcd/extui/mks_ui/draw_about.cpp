5e44e18c041c7bd64e395d8156e5a79cb64c0623
W Z <44666893+1qaz2004@users.noreply.github.com>
2022-09-13 12:45:23 +0300

    MKS Nano V3 Adding information to the Info screen.

--- Marlin/src/lcd/extui/mks_ui/draw_about.cpp
+++ Marlin/src/lcd/extui/mks_ui/draw_about.cpp
@@ -31,7 +31,7 @@
 
 extern lv_group_t *g;
 static lv_obj_t *scr;
-static lv_obj_t *fw_type, *board;
+static lv_obj_t *fw_type, *board, *website, *date, *uuid, *protocol;
 
 enum { ID_A_RETURN = 1 };
 
@@ -49,11 +49,24 @@ void lv_draw_about() {
   scr = lv_screen_create(ABOUT_UI);
   lv_big_button_create(scr, "F:/bmp_return.bin", common_menu.text_back, BTN_X_PIXEL * 3 + INTERVAL_V * 4, BTN_Y_PIXEL + INTERVAL_H + titleHeight, event_handler, ID_A_RETURN);
 
-  fw_type = lv_label_create(scr, "Firmware: Marlin " SHORT_BUILD_VERSION);
-  lv_obj_align(fw_type, nullptr, LV_ALIGN_CENTER, 0, -20);
+  fw_type = lv_label_create(scr, "Firmware version: Marlin " SHORT_BUILD_VERSION);
+  lv_obj_align(fw_type, nullptr, LV_ALIGN_CENTER, 0, -100);
 
-  board = lv_label_create(scr, "Board: " BOARD_INFO_NAME);
-  lv_obj_align(board, nullptr, LV_ALIGN_CENTER, 0, -60);
+  board = lv_label_create(scr, "Board name: " BOARD_INFO_NAME);
+  lv_obj_align(board, nullptr, LV_ALIGN_CENTER, 0, -70);
+
+  website = lv_label_create(scr, "WebSite URL: " MARLIN_WEBSITE_URL);
+  lv_obj_align(website, nullptr, LV_ALIGN_CENTER, 0, -40);
+
+  uuid = lv_label_create(scr, "UUID: " DEFAULT_MACHINE_UUID);
+  lv_obj_align(uuid, nullptr, LV_ALIGN_CENTER, 0, -10);
+
+  date = lv_label_create(scr, "Firmware date: " STRING_DISTRIBUTION_DATE);
+  lv_obj_align(date, nullptr, LV_ALIGN_CENTER, 0, 20);
+
+  protocol = lv_label_create(scr, "Protocol  version: " PROTOCOL_VERSION);
+  lv_obj_align(protocol, nullptr, LV_ALIGN_CENTER, 0, 50);
+ 
 }
 
 void lv_clear_about() {
